9469c3582335e0a717275a80c1dc546d76e5d88a
Ghita Sbai <G.Sbai@aui.ma>
2023-07-24 15:51:15 +0100

    changes

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://myshelf-11f78aafd8e0.herokuapp.com",origins],
+    allow_origins=["https://myshelf-11f78aafd8e0.herokuapp.com",origins,"http://localhost:3000/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
